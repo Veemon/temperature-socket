9bf29d0ed839d2bb98c7834905e3642416be7fd8
Veemon <xxtheforgottenliesxx@gmail.com>
2017-07-12 14:07:39 +1000

    real-time updates

--- server.py
+++ server.py
@@ -4,9 +4,14 @@ import tornado.ioloop
 import tornado.web
 import tornado.websocket
 
+from datetime import datetime
 import threading
 import time
 import os
+
+def get_date(string):
+    args = list(map(int, string.split(',')[0].split('-')))
+    return datetime(args[-1], args[-2], args[-3], args[-4], args[-5])
 
 class Index(tornado.web.RequestHandler):
     def get(self):
@@ -16,10 +21,11 @@ class Socket(tornado.websocket.WebSocketHandler):
 
     _log_path = 'gpu-temps.log'
     _sleep_timer = 5
-    _poll_timer = 1
+    _poll_timer = 5
 
     thread = 0
     slept = False
+    last_point = ['null', '']
 
     def load_data(self):
         with open(self._log_path) as f:
@@ -34,10 +40,19 @@ class Socket(tornado.websocket.WebSocketHandler):
                 self.slept = True
 
             # Poll Log File
-            #data = 'data'
+            loaded_data = self.load_data()
+            if loaded_data[-1] != self.last_point:
+                d0 = get_date(loaded_data[0])
+                d1 = get_date(self.last_point)
+                d_ = d1 - d0
+                index = int(d_.seconds/3600) + int(d_.days * 24) + 1
 
-            # Send Socket Update
-            #self.write_message(message=data)
+                # Send Socket Updates
+                if index < len(loaded_data):
+                    new_content = loaded_data[index:]
+                    self.last_point = new_content[-1]
+                    for package in new_content:
+                        self.write_message(message=package)
 
             # Recurse
             threading.Timer(self._poll_timer, self.poll_data).start()
@@ -46,7 +61,9 @@ class Socket(tornado.websocket.WebSocketHandler):
 
     def open(self):
         print("~ socket opened ~")
-        for package in self.load_data():
+        loaded_data = self.load_data()
+        self.last_point = loaded_data[-1]
+        for package in loaded_data:
             self.write_message(message=package)
         if self.thread == 0:
             self.thread = threading.Timer(self._poll_timer, self.poll_data)
@@ -57,6 +74,7 @@ class Socket(tornado.websocket.WebSocketHandler):
         self.thread.cancel()
         self.thread = 0
         self.slept = False
+        self.last_point = ['null', '']
 
 class App(tornado.web.Application):
     def __init__(self):
